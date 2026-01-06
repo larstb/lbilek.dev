54f8d2ac67aa820be1f703ac41bd4410622ce1c2
larstb <lars.bilek@gmx.at>
2026-01-06 14:47:14 +0100

    new styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {WelcomeAnimation} from './welcome-animation/welcome-animation';
 import {environment} from '../../environment';
+import {Header} from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [WelcomeAnimation],
-  templateUrl: './app.html'
+  imports: [WelcomeAnimation, Header],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
 })
 export class App {
   showSplash = environment.showSplashAnimation;
